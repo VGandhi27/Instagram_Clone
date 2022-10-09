d934adfd90e5d0551d3301b932783bd6b1392d0d
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-09 22:31:40 +0530

    mongo

--- Server/routes/Auth.js
+++ Server/routes/Auth.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const router =express.Router()
-
+const mongoose=require('mongoose')
+const User=mongoose.model("User")
 
 
 router.get('/',(req,res)=>{
@@ -13,8 +14,30 @@ router.post('/signup',(req,res)=>{
    if(!email||!password||!name){
      return res.status(422).json({error:"Please add all the Credential"})
    }
-   res.status(200).json({message:"Successfully Posted"})
+ User.findOne({email:email})
+ .then((savedUser)=>{
+    if(savedUser){
+        return res.status(422).json({error:"User already Exists with that email"})
 
+    }
+    const user=new User({
+        email,
+        password,
+        name
+    })
+    user.save()
+    .then(user=>{
+         res.json({message:"saved successfully"})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+ })
+ .catch(err=>{
+    console.log(err)
+ })
+ 
 })
 
 module.exports=router
