ef9e4777a86f4e73768013b1ddc067b85261dffa
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-10 21:50:58 +0530

    signim

--- Server/routes/Auth.js
+++ Server/routes/Auth.js
@@ -42,4 +42,31 @@ router.post('/signup',(req,res)=>{
  
 })
 
+router.post('/signin',(req,res)=>{
+   
+    const {email,password}=req.body
+    if(!email||!password){
+      return res.status(422).json({error:"Please add all the Credential"})
+    }
+
+    User.findOne({email:email})
+   .then((savedUser)=>{
+    if(!savedUser){
+        return res.status(422).json({error:"Invalid Email or Password"})
+    } 
+    bcrypt.compare(password,savedUser.password)
+    .then(doMatch=>{
+        if(doMatch){
+            res.json({message:"Successfully Signed In"})
+        }
+        else{
+            return res.status(422).json({error:"Invalid Email or Password"})
+
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+   })
+})
 module.exports=router
