3d76f603901dfb1e72528965bfe68f298810ccb5
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-11 06:08:21 +0530

    token generate

--- Server/Key.js
+++ Server/Key.js
@@ -1,3 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://Vidushi:EUltk96CrFcClKCi@cluster0.u1sord4.mongodb.net/?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://Vidushi:EUltk96CrFcClKCi@cluster0.u1sord4.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"gdjhvvdhjdbvxvjLsasdhpdbsbshshsbwey"
+
 }
--- Server/routes/Auth.js
+++ Server/routes/Auth.js
@@ -3,7 +3,8 @@ const router =express.Router()
 const mongoose=require('mongoose')
 const User=mongoose.model("User")
 const bcrypt=require("bcryptjs")
-
+const jwt=require('jsonwebtoken')
+const { JWT_SECRET }=require("../Key")
 
 router.post('/signup',(req,res)=>{
    const {name,email,password}=req.body
@@ -57,7 +58,10 @@ router.post('/signin',(req,res)=>{
     bcrypt.compare(password,savedUser.password)
     .then(doMatch=>{
         if(doMatch){
-            res.json({message:"Successfully Signed In"})
+           // res.json({message:"Successfully Signed In"})
+          const token=jwt.sign({_id:savedUser._id },JWT_SECRET)
+          res.json({token})
+        
         }
         else{
             return res.status(422).json({error:"Invalid Email or Password"})
