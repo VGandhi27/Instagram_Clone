96d122214782b1af80e49b9b081262feb7c7e0f6
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-13 17:30:07 +0530

    all post

--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -36,4 +36,17 @@ router.post("/createpost",requireLogin,(req,res)=>{
     })
 })
 
+router.get("/mypost",requireLogin,(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("postedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+
+
 module.exports =router
