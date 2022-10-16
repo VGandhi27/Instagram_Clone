ebe42b7e14df03ddd20f45def813b3891c47971b
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-16 20:03:14 +0530

    creating home

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,8 +2,48 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home Page
+    <div className='home'>
+      <div className="card home-card">
+         <h5>Vidushi Gandhi</h5>
+      <div className="card-image">
+        <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="" />
+      </div>
+       <div className="card-content">
+       <i className="material-icons" style={{color:'red'}}>favorite</i>
+        <h6>title</h6>
+        <p>This is Excellent</p>
+        <input type="text" placeholder='add a comment' />
+       </div>
+      </div>
+
+
+      <div className="card home-card">
+         <h5>Vidushi Gandhi</h5>
+      <div className="card-image">
+        <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="" />
+      </div>
+       <div className="card-content">
+       <i className="material-icons" style={{color:'red'}}>favorite</i>
+        <h6>title</h6>
+        <p>This is Excellent</p>
+        <input type="text" placeholder='add a comment' />
+       </div>
+      </div>
+
+
+      <div className="card home-card">
+         <h5>Vidushi Gandhi</h5>
+      <div className="card-image">
+        <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="" />
+      </div>
+       <div className="card-content">
+       <i className="material-icons" style={{color:'red'}}>favorite</i>
+        <h6>title</h6>
+        <p>This is Excellent</p>
+        <input type="text" placeholder='add a comment' />
+       </div>
+      </div>
+
     </div>
   )
 }
