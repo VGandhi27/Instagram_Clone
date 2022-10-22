7298515ed06dae966fd68dca17bf1e91a87ca2a0
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-22 22:32:48 +0530

    add cloudinary

--- Server/models/Post.js
+++ Server/models/Post.js
@@ -11,7 +11,7 @@ const postSchema =new mongoose.Schema({
     },
     photo:{
         type:String,
-        default:"no photo"
+        required:true
     },
     postedBy:{
         type:ObjectId,
--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -17,14 +17,15 @@ router.get("/allpost",(req,res)=>{
 
 router.post("/createpost",requireLogin,(req,res)=>{
 
-    const {title,body  }=req.body
-    if(!title ||!body){
+    const {title,body,pic  }=req.body
+    if(!title ||!body||!pic){
         return res.status(422).json({error:"Please add all the fields"})
     }
        req.user.password=undefined
     const post=new Post({
         title,
         body,
+        pic,
         postedBy:req.user
     })
     post.save().then(result=>{
