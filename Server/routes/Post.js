fe2205a410ada2717a846916de078dd8f34a31dd
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-24 00:17:57 +0530

    update home

--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose')
 const requireLogin=require("../middleware/requireLogin")
 const Post =mongoose.model("Post")
 
-router.get("/allpost",(req,res)=>{
+router.get("/allpost",requireLogin,(req,res)=>{
     Post.find()
     .populate("postedBy","_id name")
     .then(posts=>{
--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,49 +1,44 @@
-import React from 'react'
+import React,{useState,useEffect}from 'react'
 
-export default function Home() {
+const Home=()=> {
+   const [data,setData]=useState([])
+   useEffect=(()=>{
+    fetch('/allpost',{
+      headers:{
+        "Authorization":"Bearer "+localStorage.getItem("jwt")
+      }
+    }).then(res=>res.json())
+    .then(result=>{
+      setData(result.posts)
+     },[])
+   })
+
   return (
     <div className='home'>
-      <div className="card home-card">
-         <h5>Vidushi Gandhi</h5>
-      <div className="card-image">
-        <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="" />
-      </div>
-       <div className="card-content">
-       <i className="material-icons" style={{color:'red'}}>favorite</i>
-        <h6>title</h6>
-        <p>This is Excellent</p>
-        <input type="text" placeholder='add a comment' />
-       </div>
-      </div>
+       {
+        data.map(item=>{
+          return(
+            <div className="card home-card" key={item._id}>
+            <h5>{item.postedBy.name}</h5>
+         <div className="card-image">
+           <img src={item.photo} alt="" />
+         </div>
+          <div className="card-content">
+          <i className="material-icons" style={{color:'red'}}>favorite</i>
+           <h6>{item.title}</h6>
+           <p>{item.body}</p>
+           <input type="text" placeholder='add a comment' />
+          </div>
+         </div>
+   
+          )
+        })
+       }
+     
 
-
-      <div className="card home-card">
-         <h5>Vidushi Gandhi</h5>
-      <div className="card-image">
-        <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="" />
-      </div>
-       <div className="card-content">
-       <i className="material-icons" style={{color:'red'}}>favorite</i>
-        <h6>title</h6>
-        <p>This is Excellent</p>
-        <input type="text" placeholder='add a comment' />
-       </div>
-      </div>
-
-
-      <div className="card home-card">
-         <h5>Vidushi Gandhi</h5>
-      <div className="card-image">
-        <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="" />
-      </div>
-       <div className="card-content">
-       <i className="material-icons" style={{color:'red'}}>favorite</i>
-        <h6>title</h6>
-        <p>This is Excellent</p>
-        <input type="text" placeholder='add a comment' />
-       </div>
-      </div>
-
+ 
     </div>
   )
 }
+
+export default Home
