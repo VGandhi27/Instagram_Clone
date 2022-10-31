60fbce804edd8e44076e220065e0d30b753f5d8a
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-31 21:04:51 +0530

    routes change for like/unlike

--- Server/models/Post.js
+++ Server/models/Post.js
@@ -13,6 +13,9 @@ const postSchema =new mongoose.Schema({
         type:String,
         required:true
     },
+    likes:[{
+       type:ObjectId,ref:"User"
+    }],
     postedBy:{
         type:ObjectId,
         ref:"User"
