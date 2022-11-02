75fb90cefd0eaf0987593b199a59d9e93559884f
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-11-02 15:17:55 +0530

    backend comment

--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -77,5 +77,25 @@ router.put('/unlike',requireLogin,(req,res)=>{
   })
 })
 
+router.put('/comment',requireLogin,(req,res)=>{
+    const comment ={
+        text:req.body.text,
+        postedBy:req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{comments:comment}
+    },{
+        new:true
+    })
+    .populate("comments.postedBy","_id name")
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+  })
+  
 
 module.exports =router
