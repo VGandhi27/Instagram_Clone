554221199a35a47e5f5bc52da9dca89fea4bf4ea
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-11-06 20:01:43 +0530

    deleting post

--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -99,5 +99,23 @@ router.put('/comment',requireLogin,(req,res)=>{
     })
 })
 
+router.delete('/deletepost/:postId',requireLogin,(req,res)=>{
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy","_id")
+    .exec((err,post)=>{
+        if(err||!post){
+            return res.status(422).json({error:err})
+        }
+        if(post.postedBy._id.toString()===req.user._id.toString()){
+         post.remove()
+         .then(result=>{
+            res.json({message:"Successfully deleted"})
+         }).catch(err=>{
+            console.log(err)
+         })
+        }
+    })
+
+})
 
 module.exports =router
--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -95,13 +95,32 @@ const makeComment=(text,postId)=>{
  })
 }
 
+const deletePost=(postid)=>{
+  fetch(`/deletepost/:${postid}`,{
+    method:"delete",
+    headers:{
+      Authorization:"Bearer "+localStorage.getItem("jwt")
+    }
+  }).then(res=>res.json())
+  .then(result=>{
+    console.log(result)
+  }) 
+}
+
   return (
     <div className='home'>
        {
        data.map(item=>{
           return(
             <div className="card home-card" key={item._id}>
-            <h5>{item.postedBy.name}</h5>
+            <h5>{item.postedBy.name} 
+          
+            {item.postedBy._id==state._id   &&
+            <i className="material-icons" style={{float:"right"}}
+         onClick={()=>{deletePost(item._id)}}
+         >delete </i>
+            } 
+           </h5>
          <div className="card-image">
            <img src={item.photo} alt="" />
          </div>
