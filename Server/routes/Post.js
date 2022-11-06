ba764eda0fe5ea96761180027e9a9385759712a0
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-11-06 22:02:00 +0530

    final commitcdeleting post

--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -103,19 +103,18 @@ router.delete('/deletepost/:postId',requireLogin,(req,res)=>{
     Post.findOne({_id:req.params.postId})
     .populate("postedBy","_id")
     .exec((err,post)=>{
-        if(err||!post){
+        if(err || !post){
             return res.status(422).json({error:err})
         }
-        if(post.postedBy._id.toString()===req.user._id.toString()){
-         post.remove()
-         .then(result=>{
-            res.json({message:"Successfully deleted"})
-         }).catch(err=>{
-            console.log(err)
-         })
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+              post.remove()
+              .then(result=>{
+                  res.json(result)
+              }).catch(err=>{
+                  console.log(err)
+              })
         }
     })
-
 })
 
 module.exports =router
--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -95,18 +95,22 @@ const makeComment=(text,postId)=>{
  })
 }
 
-const deletePost=(postid)=>{
-  fetch(`/deletepost/:${postid}`,{
-    method:"delete",
-    headers:{
-      Authorization:"Bearer "+localStorage.getItem("jwt")
-    }
+const deletePost = (postid)=>{
+  fetch(`/deletepost/${postid}`,{
+      method:"delete",
+      headers:{
+          Authorization:"Bearer "+localStorage.getItem("jwt")
+      }
   }).then(res=>res.json())
   .then(result=>{
-    console.log(result)
-  }) 
+      console.log(result)
+      const newData = data.filter(item=>{
+          return item._id !== result._id
+      })
+      setData(newData)
+  })
 }
-
+//make a delete post
   return (
     <div className='home'>
        {
