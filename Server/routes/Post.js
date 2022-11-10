c3f8fe0438571529c63a28095ec629b2f67912c8
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-11-10 16:36:01 +0530

    updating auth.js

--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -15,6 +15,17 @@ router.get("/allpost",requireLogin,(req,res)=>{
         console.log(err)
     })
 })
+router.get("/getsubpost",requireLogin,(req,res)=>{
+    Post.find({postedBy:{$in:req.user.following}})
+    .populate("postedBy","_id name")
+    .populate("comments.postedBy","_id name")
+    .then(posts=>{
+        res.json({posts})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 
 router.post("/createpost",requireLogin,(req,res)=>{
 
--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -33,6 +33,7 @@ const Profile  = ()=>{
       </div>
       <div>
         <h4>{state?state.name:"loading"}</h4>
+        <h5>{state?state.email:"loading"}</h5>
         <div style={{display:"flex", justifyContent:"space-between" ,width:"108%"}}>
           <h5>{mypics.length} posts</h5>
           <h5>{state?state.followers.length:0} followers</h5>
