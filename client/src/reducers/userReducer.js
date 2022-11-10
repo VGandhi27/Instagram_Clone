daca802371826937bd22593959af9399359d6be9
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-11-10 16:21:49 +0530

    updating reducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -7,6 +7,13 @@ export const reducer=(state,action)=>{
    if(action.type==="CLEAR"){
      return null
    }
+   if(action.type==="UPDATE"){
+     return {
+      ...state,
+      followers:action.payload.followers,
+      following:action.payload.following
+     }
+   }
   return state
-}
+} 
 
